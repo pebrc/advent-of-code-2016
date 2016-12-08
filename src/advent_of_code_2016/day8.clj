9fb3ce47b504fdacc00346e423ae9f55bee7f386
Peter Brachwitz <peter.brachwitz@gmail.com>
2016-12-08 18:10:05 +0100

    day8 part1

---
+++ src/advent_of_code_2016/day8.clj
@@ -0,0 +1,58 @@
+(ns advent-of-code-2016.day8
+  (:require [clojure.java.io :as io]
+            [clojure.string :as s]))
+
+
+(defn display [cols rows] (vec  (repeat rows (vec (repeat cols :off)))))
+
+
+(defn rotatev [v n]
+  (let [l (count v)]
+    (vec (take l (drop (- l n) (cycle v))))))
+
+
+(defn transposev [vs]
+  (vec (apply map vector vs)))
+
+(defn rotate-row [y offset d]
+  (update d  y rotatev offset ))
+
+(defn rotate-col [x offset d]
+  (transposev (rotate-row  x offset (transposev d))))
+
+(defn rect [x y d]
+  (->> (for [x (range x) y (range y)] [y x])
+       (reduce (fn [d coords] (update-in d coords (constantly :on))) d)))
+
+
+(def rect-r #"rect (\d+)x(\d+)")
+(def rotate-row-r #"rotate row y=(\d+) by (\d+)")
+(def rotate-col-r #"rotate column x=(\d+) by (\d+)")
+
+
+(defn parse-commands [input]
+  (map 
+   #(condp re-find %
+      rect-r :>> (fn [[_ x y]] (partial rect (read-string x) (read-string y)))
+      rotate-row-r :>> (fn [[_ y os]] (partial rotate-row (read-string y) (read-string os)))
+      rotate-col-r :>> (fn [[_ x os]] (partial rotate-col (read-string x) (read-string os))))
+   input))
+
+(def input
+  (-> (-> "day8.txt"
+      io/resource
+      slurp
+      s/trim
+      s/split-lines)))
+
+(defn day8-part1 []
+  (->> (reduce (fn [grid cmd] (cmd grid)) (display 50 6) (parse-commands input))
+       flatten
+       (filter #(= % :on))
+       count))
+
+
+
+
+
+
