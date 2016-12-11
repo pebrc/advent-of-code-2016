4e91bf70c6cb8394e31396329758176de4e6bab3
Peter Brachwitz <peter.brachwitz@gmail.com>
2016-12-11 21:34:26 +0100

    day11: minor optimisations

--- src/advent_of_code_2016/day11.clj
+++ src/advent_of_code_2016/day11.clj
@@ -53,30 +53,51 @@
   (and (> cur-floor n-floor)
        (some (is-a? Gen) load)))
 
+(defn up? [cur-floor n-floor]
+  (> n-floor cur-floor))
+
+(def down? (complement up?))
+
+(defn two-up-one-down? [f nf load]
+  (let [card (count load)]
+    (or (and (up? f nf) (= 2 card))
+        (and (down? f nf) (= 1 card)))))
+
 (defn next-states [s]
   (let [cur-floor (:elevator s)]
     (for [floor (next-floors cur-floor)
           load (elevator-loads (get s cur-floor))
-          :when (safe-together? (into (get s floor) load))]
+          :when (and (safe-together? (into (get s floor) load))
+                     (two-up-one-down? cur-floor floor load))]
       (-> (update s floor into load)
           (update cur-floor set/difference load)
           (assoc :elevator floor)))))
 
 (def next-states-mem  (memoize next-states))
 
+
+
+
+(defn state-hash [s]
+  (letfn [(counts [f] (sort (map (fn [[k v]] (count v)) (group-by :e f))))]    
+    (hash (reduce (fn [r [k v]]
+                    (assoc r  k (counts v))) s
+                  (select-keys s [1 2 3 4])))))
+
 (defn result-state [prev s]
   (cond
-    (contains? prev s) :loop
+    (contains? prev (state-hash s)) :prune
     (solution? s) :solved
     :else :running))
+
 
 (defn solutions [init max-steps]
   (loop [sols [] states #{init} history #{} steps 1]
     (if (or  (not-empty sols) (empty? states) (> steps max-steps))
       sols
-      (let [r (group-by (partial result-state history) (into #{} (r/mapcat next-states states)))
+      (let [r (group-by (partial result-state history) (r/mapcat next-states states))
             _ (println steps (count (:running r)))]
-        (recur (into sols (map #(assoc % :steps steps) (:solved r))) (set (:running r)) (set/union history states) (inc steps))))))
+        (recur (into sols (map #(assoc % :steps steps) (:solved r))) (set (:running r)) (into history (map state-hash states)) (inc steps))))))
 
 
 (def simple-game {:steps 0 :elevator 1 1 #{(Gen. :po) (Micro. :po)} 2 #{} 3 #{} 4 #{}})
