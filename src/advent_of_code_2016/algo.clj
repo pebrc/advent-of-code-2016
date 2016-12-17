879afe56a9e4c6014a11f216bf052480264e6288
Peter Brachwitz <peter.brachwitz@gmail.com>
2016-12-17 15:35:48 +0100

    day13 part1  w/ bfs

---
+++ src/advent_of_code_2016/algo.clj
@@ -0,0 +1,70 @@
+(ns advent-of-code-2016.algo)
+
+
+
+(defn total-cost [estimate-cost newcost step-cost-est target xy]
+  (+ newcost
+     (estimate-cost step-cost-est target xy)))
+
+(defn path-cost [node-cost cheapest-nbr]
+  (+ node-cost (:cost cheapest-nbr 0)))
+
+(defn min-by [f coll]
+  (when (seq coll)
+    (reduce (fn [min other]
+              (if (> (f min) (f other))
+                other
+                min))
+            coll)))
+
+(defn done? [target work-queue]
+  (let [_ (println "done?" (:cost target) (first work-queue) (count work-queue))]
+    (or
+     (empty? work-queue)
+     (if-let [goal (:cost target)]
+        (< goal (first (min-by first work-queue)))
+        false))))
+
+(defn astar [start-xy target neighbors estimate-cost step-est cell-cost]
+  (loop [steps 0
+         routes {}
+         work-todo (sorted-set [0 start-xy])]
+    (if (done? (get-in routes target) work-todo)
+      (let [res (get-in routes target) ]
+        [res  :steps (count (rest (:xys res))) :iterations steps])
+      (let [[_ xy :as work-item] (first work-todo)
+            rest-work-todo (disj work-todo work-item)
+            nbr-xys (neighbors xy)
+            cheapest-nbr (min-by :cost (keep #(get-in routes %) nbr-xys))
+            newcost (path-cost (cell-cost xy) cheapest-nbr)
+            oldcost (:cost (get-in routes xy))]
+        (if (and oldcost (>= newcost oldcost))
+          (recur (inc steps) routes rest-work-todo)
+          (recur (inc steps)
+                 (assoc-in routes xy
+                           {:cost newcost
+                            :xys (conj (:xys cheapest-nbr []) xy)})
+                 (into rest-work-todo
+                       (map
+                        (fn [xy]
+                          [(total-cost estimate-cost newcost step-est target xy ) xy])
+                        nbr-xys))))))))
+
+ 
+
+(defn bfs [start-xy goal? neighbors]
+  (loop [q (conj (clojure.lang.PersistentQueue/EMPTY) start-xy)
+         routes (assoc-in {} start-xy {:xys [start-xy] :dist 0})]
+    (let [curr (peek q)]
+      (if (goal? curr)
+        (get-in routes curr) 
+        (let [cur-node (get-in routes curr)
+              nbr-xys (neighbors curr)
+              new-nbrs (filter #(nil? (get-in routes %)) nbr-xys)
+              _ (println new-nbrs cur-node)]
+          (recur (into (pop q) new-nbrs)
+                 (reduce (fn [rs xy] (assoc-in
+                                      rs
+                                      xy
+                                      {:xys (conj (:xys cur-node) xy)
+                                       :dist (inc (:dist cur-node))})) routes  new-nbrs)))))))
--- src/advent_of_code_2016/day13.clj
+++ src/advent_of_code_2016/day13.clj
@@ -1,17 +1,16 @@
 (ns advent-of-code-2016.day13
-  "As seen in Joy of Clojure"
-  (:require [advent-of-code-2016.core :refer :all]))
+  (:require [advent-of-code-2016.core :refer :all]
+            [advent-of-code-2016.algo :as a]))
 
 (def input 1358)
 
-(def target [31 39])
 
-(defn wall? [[x y] magic]
-  (not= 0 (mod (Integer/bitCount (+ magic (* y y) y (* 2 x y) (* 3 x) (* x x))) 2)))
+(defn wall? [fav [x y]]
+  (not= 0 (mod (Integer/bitCount (+ fav (* y y) y (* 2 x y) (* 3 x) (* x x))) 2)))
 
-(defn cell-cost [magic xy]
-  (if (wall? xy magic)
-    1000
+(defn cell-cost [fav xy]
+  (if (wall? fav xy)
+    10000
     1))
 
 (defn neighbors [xy]
@@ -19,61 +18,21 @@
        (map #(vec (map + xy %)))
        (filter (fn [new-xy] (every? #(< -1 %) new-xy)) )))
 
+(defn non-wall-neighbors [fav xy]
+  (->> (neighbors xy)
+       (filter (complement (partial wall? fav)))))
+
 (defn estimate-cost [step-cost target [x y]]
   (Math/abs (* step-cost (- (apply + target ) x y))))
 
 
-(defn path-cost [node-cost cheapest-nbr]
-  (+ node-cost (:cost cheapest-nbr 0)))
 
-(defn total-cost [newcost step-cost-est target xy]
-  (+ newcost
-     (estimate-cost step-cost-est target xy)))
+#_(a/bfs [1 1] #(= % [7 4]) (partial non-wall-neighbors 10))
 
-(defn min-by [f coll]
-  (when (seq coll)
-    (reduce (fn [min other]
-              (if (> (f min) (f other))
-                other
-                min))
-            coll)))
+#_(a/bfs [1 1] #(= % [31 39]) (partial non-wall-neighbors input))
 
+#_ (a/astar [1 1] [31 39] neighbors  estimate-cost 900 (partial cell-cost input))
 
-(defn done? [target work-queue]
-  (let [_ (println "done?" (:cost target) (first work-queue) (count work-queue))]
-    (or
-     (empty? work-queue)
-     (if-let [goal (:cost target)]
-        (< goal (first (min-by first work-queue)))
-        false))))
-
-(defn astar [start-xy target step-est cell-cost]
-  (loop [steps 0
-         routes {}
-         work-todo (sorted-set [0 start-xy])]
-    (if (done? (get-in routes target) work-todo)
-      (let [res (get-in routes target) ]
-        [res  :steps (count (rest (:xys res))) :iterations steps])
-      (let [[_ xy :as work-item] (first work-todo)
-            rest-work-todo (disj work-todo work-item)
-            nbr-xys (neighbors xy)
-            cheapest-nbr (min-by :cost (keep #(get-in routes %) nbr-xys))
-            newcost (path-cost (cell-cost xy) cheapest-nbr)
-            oldcost (:cost (get-in routes xy))]
-        (if (and oldcost (>= newcost oldcost))
-          (recur (inc steps) routes rest-work-todo)
-          (recur (inc steps)
-                 (assoc-in routes xy
-                           {:cost newcost
-                            :xys (conj (:xys cheapest-nbr []) xy)})
-                 (into rest-work-todo
-                       (map
-                        (fn [xy]
-                          [(total-cost newcost step-est target xy ) xy])
-                        nbr-xys))))))))
-
-#_ (astar [1 1] [31 39] 900 (partial cell-cost input))
-
-#_ (astar [1 1] [7 4] 900 (partial cell-cost 10))
+#_ (a/astar [1 1] [7 4] neighbors estimate-cost 900 (partial cell-cost 10))
 
 
