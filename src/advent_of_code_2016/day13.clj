1bae3fbe6b8c51f48688910a746b3d70fbd87684
Peter Brachwitz <peter.brachwitz@gmail.com>
2016-12-19 20:04:53 +0100

    day14: print maze

--- src/advent_of_code_2016/day13.clj
+++ src/advent_of_code_2016/day13.clj
@@ -10,7 +10,7 @@
 
 (defn cell-cost [fav xy]
   (if (wall? fav xy)
-    100000
+    Integer/MAX_VALUE
     1))
 
 (defn neighbors [xy]
@@ -30,13 +30,33 @@
 
 (def target [31 39])
 
+(defn print-path [wall? p]
+  (let [[max-x  max-y]  (last (sort p))
+        rows (range (inc max-y))
+        print-row (fn [r] (apply println r (map #(cond
+                                                     (wall? [% r]) "X"
+                                                     (<= 0 (.indexOf p [% r])) "."
+                                                     :else " ") (range (inc max-x)))))]
+       (apply println " "(range (inc max-x)))
+       (map print-row rows )))
+
+
+
+#_(print-path (partial wall? input) (:xys (first (a/astar [1 1] target neighbors  estimate-cost 1 (partial cell-cost input)))))
+
+#_(print-path (partial wall? input) (:xys (get-in  (a/bfs [1 1] (partial target? target) (partial non-wall-neighbors input)) target)))
+
+#_(print-path (partial wall? 10) [[1 1] [1 2] [2 2] [3 2] [3 3] [3 4] [4 4] [4 5] [5 5] [6 5] [7 5] [7 4]])
+
 #_ (get-in (a/bfs [1 1] (partial target? [7 4]) (partial non-wall-neighbors 10)) [7 4])
 
 #_ (get-in (a/bfs [1 1] (partial target? target) (partial non-wall-neighbors input)) target)
 
-#_ (a/astar [1 1] [31 39] neighbors  estimate-cost 900 (partial cell-cost input))
 
-#_ (a/astar [1 1] [7 4] neighbors estimate-cost 900 (partial cell-cost 10))
+#_ (a/astar [1 1] target neighbors  estimate-cost 1 (partial cell-cost input))
+
+#_ (a/astar [1 1] [7 4] neighbors estimate-cost 1 (partial cell-cost 10))
+
 
 
 ;;part2
