ec8095f9da3290ecd174448c4e7683b8cf23a5c2
Peter Brachwitz <peter.brachwitz@gmail.com>
2016-12-21 22:16:10 +0100

    day17 part1 + 2

--- src/advent_of_code_2016/algo.clj
+++ src/advent_of_code_2016/algo.clj
@@ -19,34 +19,37 @@
 
 (defn done? [target work-queue]
   (or
-   (empty? work-queue)
+   (empty? (spy work-queue))
    (when-let [goal (:cost target)]
      (< goal (first (min-by first work-queue))))))
 
-(defn astar [start-xy target neighbors estimate-cost step-est cell-cost]
-  (loop [steps 0
-         routes {}
-         work-todo (sorted-set [0 start-xy])]
-    (if (done? (get-in routes target) work-todo)
-      (let [res (get-in routes target) ]
-        [res  :steps (count (rest (:xys res))) :iterations steps])
-      (let [[_ xy :as work-item] (first work-todo)
-            rest-work-todo (disj work-todo work-item)
-            nbr-xys (neighbors xy)
-            cheapest-nbr (min-by :cost (keep #(get-in routes %) nbr-xys))
-            newcost (path-cost (cell-cost xy) cheapest-nbr)
-            oldcost (:cost (get-in routes xy))]
-        (if (and oldcost (>= newcost oldcost))
-          (recur (inc steps) routes rest-work-todo)
-          (recur (inc steps)
-                 (assoc-in routes xy
-                           {:cost newcost
-                            :xys (conj (:xys cheapest-nbr []) xy)})
-                 (into rest-work-todo
-                       (map
-                        (fn [xy]
-                          [(total-cost estimate-cost newcost step-est target xy ) xy])
-                        nbr-xys))))))))
+(defn astar
+  ([start-xy target neighbors estimate-cost step-est cell-cost]
+   (astar start-xy target identity neighbors estimate-cost step-est cell-cost))
+  ([start-state target coords neighbors estimate-cost step-est cell-cost]
+   (loop [steps 0
+          routes {}
+          work-todo (sorted-set [0 start-state])]
+     (if (done? (get-in routes target) work-todo)
+       (let [res (get-in routes target) ]
+         [res :steps (count (rest (:xys res))) :iterations steps])
+       (let [[_ loc :as work-item] (first work-todo)
+             rest-work-todo (disj work-todo work-item)
+             nbr-xys (neighbors loc)
+             cheapest-nbr (min-by :cost (keep #(get-in routes (coords %)) nbr-xys))
+             newcost (path-cost (cell-cost loc) cheapest-nbr)
+             oldcost (:cost (get-in routes (coords loc)))]
+         (if (and oldcost (>= newcost oldcost))
+           (recur (inc steps) routes rest-work-todo)
+           (recur (inc steps)
+                  (assoc-in routes (coords loc)
+                            {:cost newcost
+                             :xys (conj (:xys cheapest-nbr []) loc)})
+                  (into rest-work-todo
+                        (map
+                         (fn [loc]
+                           [(total-cost estimate-cost newcost step-est target loc ) loc])
+                         nbr-xys)))))))))
 
  
 
@@ -56,10 +59,10 @@
   ([start-xy goal? neighbors coords]
            (loop [q (conj (clojure.lang.PersistentQueue/EMPTY) start-xy)
                   routes (assoc-in {} (coords start-xy) {:xys [start-xy] :dist 0})]
-             (let [cur (spy (peek q))
+             (let [cur (peek q)
                    cur-xy (coords cur)
                    cur-node (get-in routes cur-xy)]
-               (if (goal? cur cur-node )
+               (if (or (nil? cur) (goal? cur cur-node ))
                  routes
                  (let [nbr-xys (neighbors cur)
                        new-nbrs (filter #(nil? (get-in routes (coords %))) nbr-xys)]
--- src/advent_of_code_2016/day17.clj
+++ src/advent_of_code_2016/day17.clj
@@ -3,8 +3,6 @@
             [advent-of-code-2016.algo :as a]))
 
 
-(def test-input "hijkl")
-
 (defn open? [c]
   (case c
     (\b \c \d \e \f) true
@@ -20,15 +18,36 @@
        (map (fn [[offset state]] (vector (mapv + xy offset) state)))
        (filter (fn [new-xy] (every? #(< -1 % size) (first new-xy))) )))
 
+(def target [3 0])
 
 (defn day17-part1 [secret]
-  (-> (a/bfs [[0 3] secret] (fn [[xy _] _] (= xy [3 0])) (partial neighbors 4) first)
-      (get-in [3 0])
-      :xys
-      last
-      second
-      (subs (count secret))))
-
-#_(day17-part1 "kglvqrro")
+  (-> (a/bfs [[0 3] secret] (fn [[xy _] _] (= xy target)) (partial neighbors 4) flatten)
+      (get-in target)
+      keys
+      first
+     (subs (count secret))))
 
 
+#_ (= "DDUDRLRRUDRD" (day17-part1 "kglvqrro"))
+
+#_ (= "DRURDRUDDLLDLUURRDULRLDUUDDDRR" (day17-part1 "ulqzkmiv"))
+
+#_ (day17-part1 "rrrbmfta")
+
+
+(defn stop-target [nb [xy state]]
+  (if (= xy target)
+    []
+    (nb [xy state])))
+
+(defn day17-part2 [secret]
+  (->> (keys (get-in (a/bfs
+                      [[0 3] secret]
+                      (constantly false)
+                      (partial stop-target
+                               (partial neighbors 4))
+                      flatten) target))
+       (map #(count (subs % (count secret))))
+       (reduce max)))
+
+#_(day17-part2 "rrrbmfta")
